58164bebaa459237d7a3c44d42e98ea882071f31
Oleksandr Shyshenok <shyshenok81@gmail.com>
2026-05-04 18:26:44 +0300

    add first solution

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,2 +1,5 @@
-// Add the required props
-export const UserInfo = () => <>UserInfo markup</>;
+export const UserInfo = ({ user }) => (
+  <a className="UserInfo" href="mailto:Sincere@april.biz">
+    {user ? user.name : null}
+  </a>
+);
